05717af3ec91f8c77bd702f6bcd773bb1ca7e890
motoyama3330 <moto.0815.kaz@gmail.com>
2016-11-25 18:08:08 +0900

    665ページ

--- src/main/java/mrs/domain/model/Reservation.java
+++ src/main/java/mrs/domain/model/Reservation.java
@@ -2,6 +2,7 @@ package mrs.domain.model;
 
 import java.io.Serializable;
 import java.time.LocalTime;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -69,6 +70,16 @@ public class Reservation implements Serializable {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+	public boolean overlap(Reservation target) {
+		if(!Objects.equals(reservableRoom.getReservableRoomId(), target.reservableRoom.getReservableRoomId())) {
+			return false;
+		}
+		if(startTime.equals(target.startTime) && endTime.equals(target.endTime)) {
+			return true;
+		}
+		return target.endTime.isAfter(startTime) && endTime.isAfter(target.startTime);
 	}
 
 }
